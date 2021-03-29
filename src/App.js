7baab30336c4c0575a4c3721bd57beda6cdfad0b
jake-mannino <yomannino@gmail.com>
2021-03-29 10:25:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+// import { render } from 'react-dom';
 import './App.css';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="App justify-content-center text-center align-self-center d-block">
+      <div class="accordion accordion-flush d-grid justify-content-center align-self-center d-block" id="accordionFlushExample">
+        <div class="accordion-item d-block">
+          <h2 class="accordion-header d-block" id="flush-headingOne">
+            <button class="accordion-button collapsed justify-content-center text-center align-self-center" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+              Projects
+      </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+              <div class="d-grid gap-1 d-block">
+                <button class="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                  Tic Tac Toe
+  </button>
+              </div>
+              <br></br>
+              <div class="souls">
+                <h2>gib</h2>
+              </div>Funk Raptor, the Rick James of Pokemon, is the evolved form of disco tortos a distant cousin of Groove Turtle. which is intended to demonstrate the .accordion-flush class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.
+              <div class="matrixDog"> <p>cheese</p></div>
+</div>
+          </div>        
+      </div>
+      <div class="accordion-item d-block">
+        <h2 class="accordion-header d-block" id="flush-headingTwo">
+          <button class="accordion-button collapsed justify-content-center text-center align-self-center" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+            Posts
+      </button>
+        </h2>
+        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <div class="d-grid gap-1 d-block" >
+              <button class="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                Week 5
+  </button>
+            </div>
+            <br></br>
+            <div class="container d-block">
+              <div></div>
+              <h1> <i>*Thinking critically*</i></h1>
+              <div></div>
+            </div>
 
+            Funk Raptor, the Rick James of Pokemon, is the evolved form of disco tortos a distant cousin of Groove Turtle. which is intended to demonstrate the .accordion-flush class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.
+            <div class="where"><p>we are agree</p></div>
+            <p>Chuck tenderloin jowl chislic filet mignon. Short loin short ribs doner, alcatra picanha capicola leberkas. Meatball drumstick tri-tip fatback kielbasa landjaeger porchetta. Beef turkey chuck venison pork burgdoggen pastrami frankfurter porchetta shoulder tongue strip steak doner. Ball tip jowl corned beef ground round rump tongue shankle chicken. Strip steak ball tip tail, landjaeger andouille shoulder bacon chislic pancetta.
+
+Fatback rump cupim kevin tri-tip beef ribs. Shoulder beef strip steak tri-tip ribeye andouille venison pork loin kevin shank tenderloin. Doner strip steak porchetta venison bresaola corned beef capicola spare ribs biltong kielbasa tri-tip pork belly. Sausage tenderloin salami jowl bresaola landjaeger flank kevin pig. Shoulder pork ribeye short ribs meatloaf.</p>
+    <div class="exist"></div>
+</div>
+        </div>
+      </div>
+      <div class="accordion-item d-block">
+        <h2 class="accordion-header d-block" id="flush-headingThree">
+          <button class="accordion-button collapsed justify-content-center text-center align-self-center" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+            About Me
+      </button>
+        </h2>
+        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+          <div class="accordion-body">
+            <div class="d-grid gap-1 d-block" >
+              <button class="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                Contact Me
+  </button>
+            </div> <p class="text-white">Funk Raptor, the Rick James of Pokemon, is the evolved form of disco tortos a distant cousin of Groove Turtle.  which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</p></div>
+          <div class="funkRaptor">
+            <br></br>
+            <div> <h1> watch out!</h1>
+              <img url="./imgs/funkRaptor.jpg" alt="funk raptor"></img>
+            </div>
+          </div>
+          <p>yee</p>
+        </div>
+      </div>
+    </div>
+    </div >
+  );
+};
+// function render () {
+//     return App ();
+// }
 export default App;
+// export Card;
+// export Accordion;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+// ReactDOM.render(<Header/>,document.getElementsByClassName('header'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
